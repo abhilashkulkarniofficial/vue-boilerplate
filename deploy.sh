3e1170464b07dcdf10b94d1332c0210c62500e30
Abhilash Kulkarni <abhilashkulkarniofficial@gmail.com>
2021-05-05 21:25:35 +0530

    Changed git repo name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com/abhilashkulkarniofficial/vue-boilerplate-app.git master:gh-pages
+git push -f git@github.com/abhilashkulkarniofficial/vue-boilerplate.git master:gh-pages
 cd -
